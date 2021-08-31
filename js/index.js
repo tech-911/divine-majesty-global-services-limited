4499544b51b44792666fd13dd13d483f16e16616
tech-911 <babatundeolatunji74@gmail.com>
2021-08-31 22:26:08 +0100

    working on signup page

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function signup(){
+    location.assign('../../signUp-page.html');
+
+}
+
+// document.getElementById("myButton").onclick = function () {
+//     location.href = "www.yoursite.com";
+// };
